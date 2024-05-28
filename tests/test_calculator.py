b89f24d5e7db7134936313902b7d10b9816fa2c4
sh77ma <129906512+sh77ma@users.noreply.github.com>
2024-05-28 18:47:37 +0300

    test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,16 +1,42 @@
-from src import calculator
+import unittest
+from calculator import *
 
-def test_sum():
-  assert calculator.sum(1, 2) == 3
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        clear()
 
-def test_subtract():
-  assert calculator.subtract(5, 3) == 2
+    def test_addition(self):
+        ui.label.setText("2")
+        add()
+        ui.label.setText("3")
+        equal()
+        self.assertEqual(ui.label.text(), "5.0")
 
-def test_multiply():
-  assert calculator.multiply(4, 2) == 8
+    def test_subtraction(self):
+        ui.label.setText("5")
+        subtract()
+        ui.label.setText("2")
+        equal()
+        self.assertEqual(ui.label.text(), "3.0")
 
-def test_divide():
-  assert calculator.divide(10, 2) == 5
+    def test_multiplication(self):
+        ui.label.setText("2")
+        multiply()
+        ui.label.setText("3")
+        equal()
+        self.assertEqual(ui.label.text(), "6.0")
 
-def test_divide_by_zero():
-  assert calculator.divide(10, 0) == "Деление на ноль невозможно"
+    def test_division(self):
+        ui.label.setText("6")
+        divide()
+        ui.label.setText("2")
+        equal()
+        self.assertEqual(ui.label.text(), "3.0")
+
+    def test_clear(self):
+        ui.label.setText("123")
+        clear()
+        self.assertEqual(ui.label.text(), "0")
+
+if __name__ == "__main__":
+    unittest.main()
